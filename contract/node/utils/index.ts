bc912d2affa7f5069b8de92b63499d589a0825a2
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2025-03-03 17:36:10 +0530

    Update index.ts

--- contract/node/utils/index.ts
+++ contract/node/utils/index.ts
@@ -5,6 +5,7 @@ function generateRandomString(length: number) {
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     randomString += charset[randomIndex];
+    console.log(randomString);
   }
   return randomString;
 }
